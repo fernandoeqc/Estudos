979fa45a00e4a285cf30339bd29c3d46a1d31ec0
Fernando C <fernandoeqc@live.com>
2020-12-03 09:29:27 -0300

    runtasks

--- tasks.c
+++ tasks.c
@@ -3,14 +3,21 @@
 #include <string.h>
 #include "tasks.h"
 
-
+void funcao1 () {
+   printf("Esta e a funcao 1");
+}
 
 int main () {
    static taskFunc imprime;
    imprime.active = FALSE;
+   imprime.count_sec = 0;
+   imprime.sec = 0;
+   imprime.func_time = funcao1;
 
+   
+   addTask(&imprime);
 
-   printf("hi");
+   runTasks();
    return EXIT_SUCCESS;
 }
 
