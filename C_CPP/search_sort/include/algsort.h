1d00b5e369eb4a803278da55bcf824527f9e2bb3
Fernando C <fernandoeqc@live.com>
2023-01-09 22:09:06 -0300

    update

--- C_CPP/search_sort/include/algsort.h
+++ C_CPP/search_sort/include/algsort.h
@@ -4,3 +4,4 @@ void print_vector (char range[], int len_range);
 void print_chart (char range[], int len_range);
 void print_static_chart(int j);
 void selection_sort (char range[], int len_range);
+void bubble_sort (char range[], int len_range);
--- C_CPP/search_sort/src/algsort.c
+++ C_CPP/search_sort/src/algsort.c
@@ -19,8 +19,8 @@ void print_dinam_chart (char range[], int len_range, int major, int minor)
     system("clear");
     for (int i = 0; i < len_range; i++)
     {
-        if (major == i) { s = ">"; }
-        else if (minor == i) { s = "<"; }
+        if (major == i) { s = "x"; }
+        else if (minor == i) { s = "="; }
         else { s = "|"; }
 
         printf("|");
@@ -30,7 +30,7 @@ void print_dinam_chart (char range[], int len_range, int major, int minor)
         }
         printf("|%d\n", range[i]);
     }
-    usleep(200 * 1000);
+    usleep(20 * 1000);
 }
 
 void print_static_chart(int j)
@@ -45,20 +45,54 @@ void print_static_chart(int j)
     printf("|");
 }
 
+void swap(char *a, char *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
 void selection_sort (char range[], int len_range)
 {
+    int smalest = 0;
     for (int i = 0; i < len_range; i++)
     {
         for (int j = i+1; j < len_range; j++)
         {
-            if (range[i] > range[j])
+            if (range[smalest] > range[j])
             {
-                char tmp = range[i];
-                range[i] = range[j];
-                range[j] = tmp;
+                smalest = j;
             }
-            // print_dinam_chart(range, len_range, i, j);
-            print_static_chart(j);
+            print_dinam_chart(range, len_range, i, j);
+            // print_static_chart(j);
         }
+        swap(&range[i], &range[smalest]);
+    }
+}
+
+void bubble_sort (char range[], int len_range)
+{
+    int minor = 0, major = len_range;
+    int last_major = len_range;
+    int first_minor = 0;
+
+    for (int i = 0; i < len_range; i++)
+    {
+        int tmp_last_major = 0;
+        for (int j = minor; j < last_major-1; j++)
+        {
+            if (range[j] > range[j+1])
+            {
+                swap(&range[j], &range[j+1]);
+                if (first_minor == 0)
+                {
+                    first_minor = 1;
+                    minor = j;
+                }
+            }
+            tmp_last_major = j+1;
+            print_dinam_chart(range, len_range, j, j+1);
+            // print_static_chart(j);
+        }
+        last_major = tmp_last_major;
     }
 }
